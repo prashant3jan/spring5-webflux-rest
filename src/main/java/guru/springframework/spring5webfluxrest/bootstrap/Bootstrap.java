8dff47f85a32e07e10888903db9865d3879fbf23
prashant3jan <prashant3jan@gmail.com>
2023-02-09 16:24:16 +0530

    completing assignment

---
+++ src/main/java/guru/springframework/spring5webfluxrest/bootstrap/Bootstrap.java
@@ -0,0 +1,45 @@
+package guru.springframework.spring5webfluxrest.bootstrap;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.domain.Vendor;
+import guru.springframework.spring5webfluxrest.repository.CategoryRepository;
+import guru.springframework.spring5webfluxrest.repository.VendorRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+    private final CategoryRepository categoryRepository;
+    private final VendorRepository vendorRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository, VendorRepository vendorRepository) {
+        this.categoryRepository = categoryRepository;
+        this.vendorRepository = vendorRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (categoryRepository.count().block()==0){
+            //load data
+            System.out.println("###LOADING DATA ON BOOTSTRAP");
+
+            categoryRepository.save(Category.builder().description("Fruits").build()).block();
+            categoryRepository.save(Category.builder().description("Nuts").build()).block();
+            categoryRepository.save(Category.builder().description("Breads").build()).block();
+            categoryRepository.save(Category.builder().description("Meats").build()).block();
+            categoryRepository.save(Category.builder().description("Egg").build()).block();
+
+            System.out.println("Loaded Categories: " + categoryRepository.count().block());
+
+            vendorRepository.save(Vendor.builder().firstName("Joe").lastname("Buck").build()).block();
+            vendorRepository.save(Vendor.builder().firstName("Micheal").lastname("Weston").build()).block();
+            vendorRepository.save(Vendor.builder().firstName("Jessie").lastname("Waters").build()).block();
+            vendorRepository.save(Vendor.builder().firstName("Bill").lastname("Nershi").build()).block();
+            vendorRepository.save(Vendor.builder().firstName("Jimmy").lastname("Buffett").build()).block();
+
+            System.out.println("Loaded Vendors: "+ vendorRepository.count().block());
+
+        }
+
+    }
+}
--- src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
@@ -6,13 +6,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Document
-public class Category {
+public class Vendor {
     @Id
     private String id;
-    private String description;
+    private String firstName;
+    private String lastname;
 }
